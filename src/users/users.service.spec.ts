20cc6cbe0605f8cdc0b1f9733c7f08a28460b01a
LilHeizy <vishnyakov98@gmail.com>
2024-06-11 00:05:39 +0200

    create user; get user by id - spec file tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,6 +3,8 @@ import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 const mockUser: User = {
   id: 'testId',
@@ -22,11 +24,12 @@ const mockUsersRepository = () => ({
   delete: jest.fn(),
   save: jest.fn(),
   findOne: jest.fn(),
+  findOneBy: jest.fn(),
 });
 
 describe('UsersService', () => {
   let usersService: UsersService;
-  let usersRepository;
+  let userRepository;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -37,13 +40,41 @@ describe('UsersService', () => {
     }).compile();
 
     usersService = module.get<UsersService>(UsersService);
-    usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
-
+  describe('Create User', () => {
+    it('calls a userService.createUser() should create a new user and return the new user', async () => {
+      const createUserDto: CreateUserDto = {
+        userName: 'testUsername',
+        password: 'testPassword',
+      };
+      const newUser: User = {
+        id: 'myTestUserId',
+        tasks: [],
+        ...createUserDto,
+      };
+      userRepository.save.mockResolvedValue({
+        id: 'myTestUserId',
+        tasks: [],
+        ...createUserDto,
+      });
+      const result = await usersService.createUser(createUserDto);
+      users.push(result);
+      expect(JSON.stringify(result)).toEqual(JSON.stringify(newUser));
+    });
+  });
   describe('getUsers', () => {
     it('calls a usersService.getUsers and return the result', async () => {
       const result = await usersService.getUsers();
-      expect(result.length).toEqual(0);
+      expect(result.length).toEqual(1);
+    });
+  });
+
+  describe('getUserById', () => {
+    it('calls a userService.getUserById and return a user', async () => {
+      await expect(usersService.getUserById('testId')).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
